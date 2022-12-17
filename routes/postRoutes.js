7b26a435e803f81d4905ce2886a5b28b49f3bf65
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2022-12-17 23:44:07 +0530

    Date Updated

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,17 +4,17 @@ const bodyparser = require('body-parser')
 const cloudinary = require("../cloudinary");
 const uploader = require("../multer");
 const UserArr = require("../Mock_data/data")
-
+// const date = require('date-and-time');
 const router = express.Router()
 
 router.use(bodyparser.json())
 
 router.get("/getdata", async (req, res) => {
     try {
-        const postUser = await PostData.find().sort({Date:-1})
+        const postUser = await PostData.find()
         res.status(200).json(postUser)
-            // status: "Successfully data display",
-            // result: postUser
+        // status: "Successfully data display",
+        // result: postUser
         // })
     }
     catch (e) {
@@ -25,34 +25,35 @@ router.get("/getdata", async (req, res) => {
 })
 
 router.post("/form", uploader.single("file"), async (req, res) => {
-   
+
     try {
         const Users = await PostData.find()
-        if(!Users.length){
+        if (!Users.length) {
             await PostData.create(UserArr)
         }
-        else{
-            const date = require('date-and-time');
-            const pattern = date.compile('DD MMM YYYY');
-            const value= date.format(new Date(), pattern);
-        
+        else {
+            const now = new Date();
+            const pattern = date.compile('MMM DD YYYY');
+            date.format(now, pattern);
+
+
             const upload = await cloudinary.v2.uploader.upload(req.file.path);
             // console.log(req.file)
             const data = await PostData.insertMany({
                 Author: req.body.Author,
                 Location: req.body.Location,
                 Likes: Math.floor(Math.random() * 1000),
-                Description: req.body. Description,
+                Description: req.body.Description,
                 file: upload.secure_url,
                 Date: value
             })
             console.log(Date)
-            return res. status(200).json({
+            return res.status(200).json({
                 success: true,
                 result: data
             });
         }
-        
+
     }
     catch (e) {
         res.status(404).json({
